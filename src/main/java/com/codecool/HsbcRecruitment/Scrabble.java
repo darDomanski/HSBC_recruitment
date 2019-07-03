a24f10f42b8fc42f94e85667cfec119c4318eb50
dardomanski <dardomanski@gmail.com>
2019-07-03 11:19:52 +0200

    Initialize Scrabble class

---
+++ src/main/java/com/codecool/HsbcRecruitment/Scrabble.java
@@ -0,0 +1,14 @@
+package com.codecool.HsbcRecruitment;
+
+public class Scrabble {
+    private String aWord;
+
+    public Scrabble(String aWord) {
+        this.aWord = aWord;
+    }
+
+
+    public int getScore() {
+        return 0;
+    }
+}
